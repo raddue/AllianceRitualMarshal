fb296054bf7ca68d648140bdc8806483492f2a75
raddue <eric.raddue@gmail.com>
2019-09-11 16:40:04 -0700

    Changed FAB message to a Toast.  It was a lame chance, not going to lie.

--- app/src/main/java/com/radaedalus/allianceritualmarshal/MainActivity.kt
+++ app/src/main/java/com/radaedalus/allianceritualmarshal/MainActivity.kt
@@ -2,6 +2,7 @@ package com.radaedalus.allianceritualmarshal
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,9 +19,8 @@ class MainActivity : AppCompatActivity() {
         tabs_main.setupWithViewPager(viewpager_main)
 
         val fab: View = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Add countdown and roll features here", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
+        fab.setOnClickListener {
+            Toast.makeText(applicationContext, "Add countdown and roll features here", Toast.LENGTH_LONG)
                 .show()
         }
     }
