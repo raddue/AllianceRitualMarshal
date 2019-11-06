535b533f7830092fe5cce430a962d71cf440ccf5
Rick Raddue <eric.raddue@gmail.com>
2019-11-05 16:01:34 -0800

    Removed toasts.

--- app/src/main/java/com/radaedalus/allianceritualmarshal/TimerActivity.kt
+++ app/src/main/java/com/radaedalus/allianceritualmarshal/TimerActivity.kt
@@ -3,7 +3,10 @@ package com.radaedalus.allianceritualmarshal
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TimerActivity : AppCompatActivity() {
 
@@ -11,6 +14,7 @@ class TimerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.timer_main)
 
+        initFabMenu()
     }
 
     companion object {
@@ -19,4 +23,23 @@ class TimerActivity : AppCompatActivity() {
         }
     }
 
+    private fun initFabMenu() {
+
+        val fabMain: View = findViewById<FloatingActionButton>(R.id.fab_main)
+        fabMain.setOnClickListener {
+            Toast.makeText(applicationContext, "Add slide out feature here", Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        val fabRoll: View = findViewById<FloatingActionButton>(R.id.fab_roll)
+        fabRoll.setOnClickListener {
+            DieRollerActivity.start(this)
+        }
+
+        val fabTimer: View = findViewById<FloatingActionButton>(R.id.fab_timer)
+        fabTimer.setOnClickListener {
+            TimerActivity.start(this)
+        }
+
+    }
 }
